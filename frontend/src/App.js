f89c904933a11d5520951642259184ff7325acd3
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2022-06-01 18:26:24 +0200

    frontend and backend running

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component }  from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+    <Routes>
+
+      <Route path="/" />
+
+      
+    </Routes>
+  </BrowserRouter>,
+      
+    </div>
+  );
+}
+
+export default App;
